b2314a8aa3c705fd04307afca35a162fd5cd9702
maksim <maksim.pyatnicin@gmail.com>
2016-10-05 00:07:09 +0300

    Task C is ready

--- Olympiad/Task_C/Program.cs
+++ Olympiad/Task_C/Program.cs
@@ -10,12 +10,25 @@ namespace Task_C
     {
         static void Main(string[] args)
         {
-            //тренировка со строками 
             // прочитай задачу, там не понятно нафига передается первый параметр (количество секций)
             // если я могу считать строку и посчитать количество элементов в строке. На что опираться на количество эл в строке
             //или на первое входное значение
+
+            int CountElem = int.Parse(Console.ReadLine());
             int[] matrFieldForce = Console.ReadLine().Split(new char[] { ' ' }).Select(x=>int.Parse(x)).ToArray();
-            Console.WriteLine(String.Join("/",matrFieldForce));
+            int indexMiddleElemForMaxSumm = 0;
+            int MaxSumm = 0;
+                
+            for (int i = 0; i < CountElem-2; i++)
+            {
+                if (MaxSumm < (matrFieldForce[i] + matrFieldForce[i + 1] + matrFieldForce[i + 2]))
+                {
+                    MaxSumm = matrFieldForce[i] + matrFieldForce[i + 1] + matrFieldForce[i + 2];
+                    indexMiddleElemForMaxSumm = i + 2;
+                }
+                
+            }
+            Console.WriteLine($"{MaxSumm} {indexMiddleElemForMaxSumm}");
         }
     }
 }
