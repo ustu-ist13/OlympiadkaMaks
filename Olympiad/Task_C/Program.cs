e35b935397267dc9e640cbcb3fce6811fbbbdf91
maksim <maksim.pyatnicin@gmail.com>
2016-10-04 23:42:13 +0300

    question to the master

---
+++ Olympiad/Task_C/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //тренировка со строками 
+            // прочитай задачу, там не понятно нафига передается первый параметр (количество секций)
+            // если я могу считать строку и посчитать количество элементов в строке. На что опираться на количество эл в строке
+            //или на первое входное значение
+            int[] matrFieldForce = Console.ReadLine().Split(new char[] { ' ' }).Select(x=>int.Parse(x)).ToArray();
+            Console.WriteLine(String.Join("/",matrFieldForce));
+        }
+    }
+}
